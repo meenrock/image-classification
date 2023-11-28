1ff35bbc2ec98642dae34be2cd9a8e75592d5f1f
sstacker_m <77928053+meenrock@users.noreply.github.com>
2023-11-28 13:07:47 +0700

    update

--- flask-web.py
+++ flask-web.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, render_template, Response
 from kafka import KafkaConsumer, KafkaProducer
+import json
+import time
 
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_SERVER, #KAFKA SERVER
@@ -17,3 +19,26 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+def stream_template(template_name, **context):
+    """Enabling streaming back results to app"""
+    app.update_template_context(context)
+    template = app.jinja_env.get_template(template_name)
+    streaming = template.stream(context)
+    return streaming
+
+@app.route('/send-rdy-message/<my_id>', methods=['POST'])
+def pizza_ready(my_id=None):
+    """Endpoint to pass ready pizzas"""
+    print(my_id)
+    producer.send(
+        TOPIC_DELIVERY_NAME,
+        key={"timestamp": my_id},
+        value=request.json
+    )
+    producer.flush()
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
--- svm_example/mcsvm.py
+++ svm_example/mcsvm.py
@@ -1,10 +1,26 @@
 from sklearn.linear_model import SGDClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
 import time
 import pandas as pd
 import urllib.request, json
 import os
 from dotenv import load_dotenv
+from nltk import wordnet as wn
+
+load_dotenv()
+
+def clean(text):
+    for punctuation in string.punctuation:
+        text = text.replace(punctuation, ' ') # Remove Punctuation
+    lowercased = text.lower() # Lower Case
+    tokenized = word_tokenize(lowercased) # Tokenize
+    words_only = [word for word in tokenized if word.isalpha()] # Remove numbers
+    stop_words = set(stopwords.words('english')) # Make stopword list
+    without_stopwords = [word for word in words_only if not word in stop_words] # Remove Stop Words
+    lemma=WordNetLemmatizer() # Initiate Lemmatizer
+    lemmatized = [lemma.lemmatize(word) for word in without_stopwords] # Lemmatize
+    return lemmatized
 
 def all_df_pexels(style_list):
     url = "".format(os.environ.get("PEXEL_API_KEY"))
@@ -36,6 +52,6 @@ sgd = Pipeline([('vect', CountVectorizer()),
                 ('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42, max_iter=5, tol=None)),
                ])
 sgd.fit(X_train, y_train)
-%time
+time
 y_pred = sgd.predict(X_test)
 print('accuracy %s' % accuracy_score(y_pred, y_test))
