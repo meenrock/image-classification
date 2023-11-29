d7df42b0a642a17b75547bbff7ed5746226495c8
sstacker_m <77928053+meenrock@users.noreply.github.com>
2023-11-29 12:57:18 +0700

    Update flask-web.py

--- flask-web.py
+++ flask-web.py
@@ -4,15 +4,15 @@ import json
 import time
 from svm_example.mcsvm import mcsvm
 
-producer = KafkaProducer(
-    bootstrap_servers=KAFKA_SERVER, #KAFKA SERVER
-    # security_protocol="SSL", 
-    # ssl_cafile=CERTS_FOLDER + "/ca.pem",
-    # ssl_certfile=CERTS_FOLDER + "/service.cert",
-    # ssl_keyfile=CERTS_FOLDER + "/service.key",
-    value_serializer=lambda v: json.dumps(v).encode('ascii'),
-    key_serializer=lambda v: json.dumps(v).encode('ascii')
-)
+# producer = KafkaProducer(
+#     bootstrap_servers=KAFKA_SERVER,
+#     security_protocol="SSL", 
+#     ssl_cafile=CERTS_FOLDER + "/ca.pem",
+#     ssl_certfile=CERTS_FOLDER + "/service.cert",
+#     ssl_keyfile=CERTS_FOLDER + "/service.key",
+#     value_serializer=lambda v: json.dumps(v).encode('ascii'),
+#     key_serializer=lambda v: json.dumps(v).encode('ascii')
+# )
 
 
 app = Flask(__name__)
@@ -21,28 +21,29 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-def stream_template(template_name, **context):
-    """Enabling streaming back results to app"""
-    app.update_template_context(context)
-    template = app.jinja_env.get_template(template_name)
-    streaming = template.stream(context)
-    return streaming
+# def stream_template(template_name, **context):
+#     """Enabling streaming back results to app"""
+#     app.update_template_context(context)
+#     template = app.jinja_env.get_template(template_name)
+#     streaming = template.stream(context)
+#     return streaming
 
-@app.route('/send-rdy-message/<my_id>', methods=['POST'])
-def pizza_ready(my_id=None):
-    """Endpoint to pass ready pizzas"""
-    print(my_id)
-    producer.send(
-        TOPIC_DELIVERY_NAME,
-        key={"timestamp": my_id},
-        value=request.json
-    )
-    producer.flush()
-    return "OK"
+# @app.route('/send-rdy-message/<my_id>', methods=['POST'])
+# def pizza_ready(my_id=None):
+#     """Endpoint to pass ready pizzas"""
+#     print(my_id)
+#     producer.send(
+#         TOPIC_DELIVERY_NAME,
+#         key={"timestamp": my_id},
+#         value=request.json
+#     )
+#     producer.flush()
+#     return "OK"
 
-@app.router('/test/mcsvm',methods=['GET'])
+@app.route('/test/mcsvm',methods=['GET'])
 def test_mcsvm():
-    mcsvm.iris_test_data_mcsvm()
+    result = mcsvm.iris_test_data_mcsvm()
+    return "OK"
 
 
 if __name__ == "__main__":
